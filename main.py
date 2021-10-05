6e16e766f6e28747f319e0602b3dc97811587ab3
linafaik08 <linafaik08@gmail.com>
2021-10-05 14:14:06 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def get_table_stats(df):
-    """Return main statistics of a dataframe df"""
+    """Return main statistics of a dataframe df."""
     table = df.describe()
     return table
